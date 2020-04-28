2c36a82181ab14a78e22c72a7a6d0e30111d8b10
Soluyanov <alexey.soluyanov@intel.com>
2020-04-28 12:02:36 +0300

    removing unnecessary buffers

--- image_blur.cpp
+++ image_blur.cpp
@@ -34,52 +34,39 @@ int main(int, char**) {
     size_t img_size = pixels_count * channels;
 
     // rgbrgb..rgb -> rr..rgg..gbb..b
-    unsigned char *r = new unsigned char[pixels_count * sizeof(unsigned char)];
-    unsigned char *g = new unsigned char[pixels_count * sizeof(unsigned char)];
-    unsigned char *b = new unsigned char[pixels_count * sizeof(unsigned char)];
     unsigned char *rgb = new unsigned char[img_size * sizeof(unsigned char)];
-    unsigned char *(r_) = r, *(g_) = g, *(b_) = b;
-    for(size_t i = 0; i < pixels_count; i++, (r_)++, (g_)++, (b_)++) {
-        *(r_) = (uint8_t)(*(img + i * channels));
-        *(g_) = (uint8_t)(*(img + i * channels + 1));
-        *(b_) = (uint8_t)(*(img + i * channels + 2));
+    for(size_t i = 0; i < pixels_count; i++) {
+        *(rgb + i) = (uint8_t)(*(img + i * channels));
+        *(rgb + pixels_count + i) = (uint8_t)(*(img + i * channels + 1));
+        *(rgb + 2 * pixels_count + i) = (uint8_t)(*(img + i * channels + 2));
     }
-    memcpy(rgb, r, pixels_count * sizeof(unsigned char));
-    unsigned char *tmp = rgb + pixels_count;
-    memcpy(tmp, g, pixels_count * sizeof(unsigned char));
-    *tmp = *(tmp + pixels_count);
-    memcpy(tmp, b, pixels_count * sizeof(unsigned char));
     /*
         Native method
     */
     unsigned char *final_image = new unsigned char[img_size * sizeof(unsigned char)];
-    unsigned char *final_image_tmp = final_image;
     unsigned char *r_curr_pix, *g_curr_pix, *b_curr_pix;
     int capacity = (filter_size - 1) / 2;
-    for(int i = capacity; i <= height - capacity - 1; i++) {
-        for(int j = capacity; j <= width - capacity - 1; j++) {
+    for(int i = 0; i <= height - 1; i++) {
+        for(int j = 0; j <= width - 1; j++) {
             vector<double> sum(3, 0);
             for(int k = (-1) * capacity; k <= capacity; k++) {
                 for(int t = (-1) * capacity; t <= capacity; t++) {
-                    r_curr_pix = r + i * width + j;
-                    g_curr_pix = g + i * width + j;
-                    b_curr_pix = b + i * width + j;
+                    if((j + t) < 0 || (i + k) < 0) {
+                        continue;
+                    }
+                    r_curr_pix = rgb + i * width + j;
+                    g_curr_pix = (rgb + pixels_count) + i * width + j;
+                    b_curr_pix = (rgb + 2* pixels_count) + i * width + j;
                     sum[0] += (uint8_t)(double(*(r_curr_pix + k * width + t)) * filter[capacity + k][capacity + t]);
                     sum[1] += (uint8_t)(double(*(g_curr_pix + k * width + t)) * filter[capacity + k][capacity + t]);
                     sum[2] += (uint8_t)(double(*(b_curr_pix + k * width + t)) * filter[capacity + k][capacity + t]);
                 }
             }
-            *final_image_tmp++ = (uint8_t)(sum[0]);
-            *final_image_tmp++ = (uint8_t)(sum[1]);
-            *final_image_tmp++ = (uint8_t)(sum[2]);
+            *(final_image + (i * width + j) * channels) = (uint8_t)(sum[0]);
+            *(final_image + (i * width + j) * channels + 1) = (uint8_t)(sum[1]);
+            *(final_image + (i * width + j) * channels + 2) = (uint8_t)(sum[2]);
             sum.clear();
         }
     }
-
-    // for(unsigned char *p = img, *pg = gray_img; p != img + img_size; p += channels, pg += gray_channels) {
-    //     *pg = (uint8_t)((*p + *(p + 1) + *(p + 2))/3.0);
-    // }
-
-    stbi_write_jpg("test_test_test.jpg", width - filter_size + 1, height - filter_size + 1, channels, final_image, 100);
-
+    stbi_write_jpg("test_test_test.jpg", width, height, channels, final_image, 100);
 }
