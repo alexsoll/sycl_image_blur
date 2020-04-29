2ed26f8c10daaa70425c2a1e488e25391f1ffb27
alexsoll <al.soluyanov@rambler.ru>
2020-04-29 23:39:40 +0300

    final version

--- image_blur.cpp
+++ image_blur.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <vector>
-#include <iostream>
+#include <cstring>
 #include <ctime>
 #include <ratio>
 #include <chrono>
+#include <fstream>
 #include <CL/sycl.hpp>
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
@@ -13,125 +14,108 @@
 using namespace cl::sycl;
 using namespace std;
 
-range<2> get_optimal_local_range(cl::sycl::range<2> globalSize,
-                                 cl::sycl::device d) {
-    range<2> optimalLocalSize(0, 0);
+int main(int agrc, char** argv) {
+    int channels;
+    static constexpr auto filterSize = 7;
+    unsigned char* inputData = nullptr;
+    unsigned char* outputData_sycl = nullptr;
+    unsigned char* outputData_native = nullptr;
+    int inputWidth, inputHeight;
 
-    if (d.is_gpu()) {
-        optimalLocalSize = range<2>(64, 1);
-    } else {
-        optimalLocalSize = range<2>(4, 1);
-    }
-
-    for (int i = 0; i < 2; ++i) {
-        while (globalSize[i] % optimalLocalSize[i]) {
-            optimalLocalSize[i] = optimalLocalSize[i] >> 1;
-        }
-    }
-    return optimalLocalSize;
-}
-
-int main(int, char**) {
-    int width, height, channels;
     //Load image
-    unsigned char *img = stbi_load("wot.jpg", &width, &height, &channels, 0);
-    if(img == NULL) {
+    inputData = stbi_load(argv[1], &inputWidth, &inputHeight, &channels, 0);
+    if(inputData == NULL) {
         std::cout << "Error in loading the image" << std::endl;
         exit(1);
     }
-    std::cout << "Loaded image with a width of " << width << "px, a height of " << height << "px and " 
-    << "and " << channels << " channels" << std::endl;
+
+    std::cout << "Loaded image with a width of " << inputWidth << "px, a height of " << inputHeight << "px and " 
+    << channels << " channels" << std::endl;
 
     // Create filters
-    size_t filter_size = 7;
-    vector<vector<double> > filter(filter_size, vector<double>(filter_size));
+    vector<vector<double> > filter(filterSize, vector<double>(filterSize));
     for (int i = 0; i < filter.size(); i++) {
         for (int j = 0; j < filter[i].size(); j++) {
-            filter[i][j] = 1.0 / (filter_size * filter_size);
+            filter[i][j] = 1.0 / (filterSize * filterSize);
         }
     }
 
-    size_t pixels_count = width * height;
-    size_t img_size = pixels_count * channels;
+    int pixCount = inputWidth * inputHeight;
+    int imgSize = pixCount * channels;
 
     // rgbrgb..rgb -> rr..rgg..gbb..b
-    unsigned char *rgb = new unsigned char[img_size * sizeof(unsigned char)];
-    for(size_t i = 0; i < pixels_count; i++) {
-        *(rgb + i) = (uint8_t)(*(img + i * channels));
-        *(rgb + pixels_count + i) = (uint8_t)(*(img + i * channels + 1));
-        *(rgb + 2 * pixels_count + i) = (uint8_t)(*(img + i * channels + 2));
+    unsigned char *rgb = new unsigned char[imgSize];
+    for(size_t i = 0; i < pixCount; i++) {
+        *(rgb + i) = (uint8_t)(*(inputData + i * channels));
+        *(rgb + pixCount + i) = (uint8_t)(*(inputData + i * channels + 1));
+        *(rgb + 2 * pixCount + i) = (uint8_t)(*(inputData + i * channels + 2));
     }
+
     /*
         Native method
     */
-    unsigned char *final_image = new unsigned char[img_size * sizeof(unsigned char)];
+    outputData_native = new unsigned char[imgSize];
     unsigned char *r_curr_pix, *g_curr_pix, *b_curr_pix;
-    int capacity = (filter_size - 1) / 2;
+    int capacity = (filterSize - 1) / 2;
     auto start_time = chrono::high_resolution_clock::now();
-    for(int i = 0; i <= height - 1; i++) {
-        for(int j = 0; j <= width - 1; j++) {
+    for(int i = 0; i <= inputHeight - 1; i++) {
+        for(int j = 0; j <= inputWidth - 1; j++) {
             vector<double> sum(3, 0);
             for(int k = (-1) * capacity; k <= capacity; k++) {
                 for(int t = (-1) * capacity; t <= capacity; t++) {
                     if((j + t) < 0 || (i + k) < 0) {
                         continue;
                     }
-                    r_curr_pix = rgb + i * width + j;
-                    g_curr_pix = (rgb + pixels_count) + i * width + j;
-                    b_curr_pix = (rgb + 2* pixels_count) + i * width + j;
-                    sum[0] += (uint8_t)(double(*(r_curr_pix + k * width + t)) * filter[capacity + k][capacity + t]);
-                    sum[1] += (uint8_t)(double(*(g_curr_pix + k * width + t)) * filter[capacity + k][capacity + t]);
-                    sum[2] += (uint8_t)(double(*(b_curr_pix + k * width + t)) * filter[capacity + k][capacity + t]);
+                    r_curr_pix = rgb + i * inputWidth + j;
+                    g_curr_pix = (rgb + pixCount) + i * inputWidth + j;
+                    b_curr_pix = (rgb + 2* pixCount) + i * inputWidth + j;
+                    sum[0] += (uint8_t)(double(*(r_curr_pix + k * inputWidth + t)) * filter[capacity + k][capacity + t]);
+                    sum[1] += (uint8_t)(double(*(g_curr_pix + k * inputWidth + t)) * filter[capacity + k][capacity + t]);
+                    sum[2] += (uint8_t)(double(*(b_curr_pix + k * inputWidth + t)) * filter[capacity + k][capacity + t]);
                 }
             }
-            *(final_image + (i * width + j) * channels) = (uint8_t)(sum[0]);
-            *(final_image + (i * width + j) * channels + 1) = (uint8_t)(sum[1]);
-            *(final_image + (i * width + j) * channels + 2) = (uint8_t)(sum[2]);
+            *(outputData_native + (i * inputWidth + j) * channels) = (uint8_t)(sum[0]);
+            *(outputData_native + (i * inputWidth + j) * channels + 1) = (uint8_t)(sum[1]);
+            *(outputData_native + (i * inputWidth + j) * channels + 2) = (uint8_t)(sum[2]);
             sum.clear();
         }
     }
     auto end_time = chrono::high_resolution_clock::now();
-    std::cout << "Native method's time: " << chrono::duration_cast<chrono::microseconds>((end_time - start_time) / 1000).count() <<
-    " microseconds" << std::endl;
-    stbi_write_jpg("wot_native_methods.jpg", width, height, channels, final_image, 100);
+    auto native_time = end_time - start_time;
+    stbi_write_jpg("native_methods.jpg", inputWidth, inputHeight, channels, outputData_native, 100);
 
+    delete[] rgb;
+    delete[] outputData_native;
 
     /*
         SYCL implementation
     */
-    static constexpr auto filterSize = 7;
-    unsigned char* inputData = nullptr;
-    unsigned char* outputData = nullptr;
-    int inputWidth, inputHeight;
-    // Load image
-    inputData = stbi_load("wot.jpg", &inputWidth, &inputHeight, &channels, 0);
+    outputData_sycl = new unsigned char[imgSize];
 
-    int imgSize = inputWidth * inputHeight * channels;
-    int pixCount = inputWidth * inputHeight;
-    // Create image and flilter range
-    range<2> imgRange(inputHeight, inputWidth);
+    // Create flilter range
     range<2> filterRange(filterSize, filterSize);
-    // Final image
-    outputData = new unsigned char[imgSize];
 
+    std::string MySelector = argv[2];
 
-    // Create queue
-    queue Queue([](cl::sycl::exception_list l) {
-    for (auto ep : l) {
-        try {
-            std::rethrow_exception(ep);
-        } catch (const cl::sycl::exception& e) {
-            std::cout << "Async exception caught:\n" << e.what() << "\n";
-            throw;
-        }
+    queue myQueue;
+
+    queue cpuQueue( cpu_selector{});
+    queue gpuQueue( gpu_selector{});
+
+    if (std::strcmp(argv[2], "cpu") == 0) {
+        myQueue = cpuQueue;
+    } else {
+        myQueue = gpuQueue;
     }
-    });
+
+
+    std::cout << "\nRunning on "
+                << myQueue.get_device().get_info<cl::sycl::info::device::name>()
+                << "\n";
 
     vector<int> imgData(imgSize);
-    for(int i = 0; i < pixCount; i++) {
-        imgData[i * channels] = (int(*(inputData + i * channels)));
-        imgData[i * channels + 1] = (int(*(inputData + i * channels + 1)));
-        imgData[i * channels + 2] = (int(*(inputData + i * channels + 2)));
+    for(int i = 0; i < imgSize; i++) {
+        imgData[i] = (int(*(inputData + i)));
     }
 
     {
@@ -139,7 +123,7 @@ int main(int, char**) {
             Create filter's kernel
         */  
         buffer<float, 2> blur(filterRange);
-        Queue.submit([&](cl::sycl::handler& cgh) {
+        myQueue.submit([&](cl::sycl::handler& cgh) {
             auto globalBlur = blur.get_access<access::mode::discard_write>(cgh);
             cgh.parallel_for<class blurFilter>(filterRange, [=](cl::sycl::item<2> i) {
                 globalBlur[i] = 1. / (filterSize * filterSize);
@@ -148,9 +132,9 @@ int main(int, char**) {
         cl::sycl::buffer<int> imgData_buffer(imgData.data(), cl::sycl::range<1>(imgSize));
         cl::sycl::buffer<int> RGB_buffer(imgSize);
 
-        Queue.submit([&](cl::sycl::handler& cgh) {
+        myQueue.submit([&](cl::sycl::handler& cgh) {
             auto globalImgBuf = imgData_buffer.get_access<access::mode::read>(cgh);
-            auto globalRGB = RGB_buffer.get_access<access::mode::write>(cgh);
+            auto globalRGB = RGB_buffer.get_access<access::mode::discard_write>(cgh);
             cgh.parallel_for<class to_rgb>(cl::sycl::range<1>(pixCount), [=](cl::sycl::nd_item<1> item) {
                 int wiID = item.get_global_id()[0];
                 globalRGB[wiID] = globalImgBuf[wiID * channels];
@@ -158,46 +142,70 @@ int main(int, char**) {
                 globalRGB[wiID + 2 * pixCount] = globalImgBuf[wiID * channels + 2];
             });
         });
-        
-        std::vector<int> finalIMG_vector(img_size, 0);
+
+        imgData.clear();
+
+        std::vector<int> finalIMG_vector(imgSize, 0);
         buffer<int> finalIMG_buffer(finalIMG_vector.data(), cl::sycl::range<1>(imgSize));
         start_time = chrono::high_resolution_clock::now();
-        Queue.submit([&](cl::sycl::handler& cgh) {
-            cl::sycl::stream kernelout(320000, 1024, cgh);
-            auto globalRGB = RGB_buffer.get_access<access::mode::read>(cgh);
-            auto globalBlur = blur.get_access<access::mode::read>(cgh);
-            auto globalFinalImg = finalIMG_buffer.get_access<access::mode::write>(cgh);
+        std::cout << "Start calculation..." << std::endl;
+        try {
+            myQueue.submit([&](cl::sycl::handler& cgh) {
+                auto globalRGB = RGB_buffer.get_access<access::mode::read>(cgh);
+                auto globalBlur = blur.get_access<access::mode::read>(cgh);
+                auto globalFinalImg = finalIMG_buffer.get_access<access::mode::discard_write>(cgh);
 
-            cgh.parallel_for<class create_final_image>(cl::sycl::range<1>(pixCount), [=](cl::sycl::nd_item<1> item) {
-                int wiID = item.get_global_id()[0];
-                constexpr auto offset = (filterSize - 1) / 2;
-                for (int x = -offset; x <= offset; x++) {
-                    for (int y = -offset; y <= offset; y++) {
-                        if(((wiID % inputWidth) + y) < 0 || (int(wiID / inputWidth) + 1 + x) < 0) {
-                            continue;
+                cgh.parallel_for<class create_final_image>(cl::sycl::range<1>(pixCount), [=](cl::sycl::nd_item<1> item) {
+                    size_t wiID = item.get_global_id()[0];
+                    auto r = 0;
+                    auto g = 0;
+                    auto b = 0;
+                    constexpr auto offset = (filterSize - 1) / 2;
+                    for (int x = -offset; x <= offset; x++) {
+                        for (int y = -offset; y <= offset; y++) {
+                            if(((wiID % inputWidth) + y) < 0 || (size_t(wiID / inputWidth) + x) < 0) {
+                                continue;
+                            }
+                            if(((wiID % inputWidth) + y) >= inputWidth || (size_t(wiID / inputWidth) + x) >= inputHeight) {
+                                continue;
+                            }
+                            size_t r_curr_pix = wiID;
+                            size_t g_curr_pix = wiID + pixCount;
+                            size_t b_curr_pix = wiID + 2 * pixCount;
+                            r += globalRGB[r_curr_pix + x * inputWidth + y] * globalBlur[offset + x][offset + y];
+                            g += globalRGB[g_curr_pix + x * inputWidth + y] * globalBlur[offset + x][offset + y];
+                            b += globalRGB[b_curr_pix + x * inputWidth + y] * globalBlur[offset + x][offset + y];
                         }
-                        auto r_curr_pix = wiID;
-                        auto g_curr_pix = wiID + pixCount;
-                        auto b_curr_pix = wiID + 2 * pixCount;
-                        globalFinalImg[wiID * channels] += globalRGB[r_curr_pix + x * inputWidth + y] * globalBlur[offset + x][offset + y];// for R
-                        globalFinalImg[wiID * channels + 1] += globalRGB[g_curr_pix + x * inputWidth + y] * globalBlur[offset + x][offset + y];// for G
-                        globalFinalImg[wiID * channels + 2] += globalRGB[b_curr_pix + x * inputWidth + y] * globalBlur[offset + x][offset + y];// for B
                     }
-                }
+                    globalFinalImg[wiID * channels] = r;
+                    globalFinalImg[wiID * channels + 1] = g;
+                    globalFinalImg[wiID * channels + 2] = b;
+                });
             });
-        });
-        end_time = chrono::high_resolution_clock::now();
-        std::cout << "Parallel method's time: " << chrono::duration_cast<chrono::microseconds>((end_time - start_time) / 1000).count() << 
-        " microseconds" << std::endl;
+            myQueue.wait_and_throw();
+        } catch (std::exception e) {
+            std::cout << e.what() << std::endl;
+        }
 
+        std::cout << "Finish calculation..." << std::endl;
+        end_time = chrono::high_resolution_clock::now();
+        auto sycl_time = end_time - start_time;
+        std::cout << std::endl << "Native method's time: " << chrono::duration_cast<chrono::microseconds>(native_time / 1000).count() <<
+        " microseconds" << std::endl;
+        std::cout << "Parallel method's time: " << chrono::duration_cast<chrono::microseconds>(sycl_time / 1000).count() << 
+        " milliseconds" << std::endl;
 
         auto globalFinalImg = finalIMG_buffer.get_access<access::mode::read>();
         for(int i = 0; i < pixCount; i++) {
-            *(outputData + i * channels) = globalFinalImg[i * channels];
-            *(outputData + i * channels + 1) = globalFinalImg[i * channels + 1];
-            *(outputData + i * channels + 2) = globalFinalImg[i * channels + 2];
+            *(outputData_sycl + i * channels) = globalFinalImg[i * channels];
+            *(outputData_sycl + i * channels + 1) = globalFinalImg[i * channels + 1];
+            *(outputData_sycl + i * channels + 2) = globalFinalImg[i * channels + 2];
         }
-            stbi_write_jpg("wot_sycl.jpg", inputWidth, inputHeight, channels, outputData, 100);
+        stbi_write_jpg("nature_sycl.jpg", inputWidth, inputHeight, channels, outputData_sycl, 100);
+    }
 
-    } 
+    // free memory
+    delete[] outputData_sycl;
+    delete[] inputData;
+    return 0;
 }
